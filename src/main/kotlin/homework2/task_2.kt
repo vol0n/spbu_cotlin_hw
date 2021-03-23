c41f6f4a15d80bc9352020154452689b9e458981
Volynets Arseniy <vol0ncar@yandex.ru>
2021-03-23 17:31:46 +0300

    Remove redunant paretheses, fix file structure in test module

--- src/main/kotlin/homework2/task_2.kt
+++ src/main/kotlin/homework2/task_2.kt
@@ -3,6 +3,6 @@ package homework2
 fun <E> MutableList<E>.removeDoubles(): MutableList<E> {
     val bag = mutableSetOf<E>()
     this.reversed()
-        .map() { x -> if (x !in bag) bag.add(x) else this.remove(x) }
+        .map { x -> if (x !in bag) bag.add(x) else this.remove(x) }
     return this
 }
--- src/test/kotlin/homework2/Task_2KtTest.kt
+++ src/test/kotlin/homework2/Task_2KtTest.kt
@@ -1,7 +1,6 @@
-package util
+package homework2
 
 import org.junit.jupiter.api.Assertions.*
-import homework2.removeDoubles
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
