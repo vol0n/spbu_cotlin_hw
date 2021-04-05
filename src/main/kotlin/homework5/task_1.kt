ef227229e9c9450855e235e4ff5ef1c8554f5935
Volynets Arseniy <vol0ncar@yandex.ru>
2021-04-05 15:24:50 +0300

    Override toSting(), instead of print()

--- src/main/kotlin/homework5/task_1.kt
+++ src/main/kotlin/homework5/task_1.kt
@@ -2,65 +2,63 @@ package homework5
 
 import java.io.File
 
-abstract class Node {
-    var left: Node? = null
-    var right: Node? = null
-    abstract fun compute(): Int
-    abstract fun print(indentWidth: Int, indentStr: String = ".")
+interface Node {
+    companion object {
+        const val indentWidth = 3
+        const val indentStr = "."
+    }
+    var height: Int
+    fun compute(): Int
 }
 
-class Operator(private val type: Char) : Node() {
+class Operator(private val type: Char, private var left: Node, private var right: Node) : Node {
+    override var height = 0
     override fun compute(): Int {
-        val l = left; val r = right
-        if (l == null || r == null) error("Operator Node must have two children!")
         return when (type) {
-            '+' -> l.compute() + r.compute()
-            '*' -> l.compute() * r.compute()
-            '/' -> l.compute() / r.compute()
-            '-' -> l.compute() - r.compute()
+            '+' -> left.compute() + right.compute()
+            '*' -> left.compute() * right.compute()
+            '/' -> left.compute() / right.compute()
+            '-' -> left.compute() - right.compute()
             else -> error("Unknown operator: $type !")
         }
     }
 
-    override fun print(indentWidth: Int, indentStr: String) {
-        println(indentStr.repeat(indentWidth) + type)
-        left?.print(indentWidth, indentStr)
-        right?.print(indentWidth, indentStr)
-    }
+    override fun toString(): String = Node.indentStr.repeat(height) + type +
+            "\n" + left.also { it.height = this.height + Node.indentWidth } +
+            "\n" + right.also { it.height = this.height + Node.indentWidth }
 }
 
-class Operand(private val value: Int) : Node() {
+class Operand(private val value: Int) : Node {
+    override var height: Int = 0
     override fun compute() = value
-    override fun print(indentWidth: Int, indentStr: String) {
-        println(indentStr.repeat(indentWidth) + value.toString())
-        left?.print(indentWidth, indentStr)
-        right?.print(indentWidth, indentStr)
-    }
+    override fun toString(): String = ".".repeat(height) + value
 }
 
 enum class Type {
     OPERATOR, OPERAND, LEFT, RIGHT
 }
 
+data class Token(val lexeme: String, val type: Type)
+
 class Tokenizer(path: String) {
     private val sourceStr: String = File(path).readText()
     private var i: Int = -1
-    fun next(): Pair<String, Type> {
+    fun next(): Token {
         while (nextChar() == ' ')
             move()
         val c = nextChar()
-        val token: Pair<String, Type> = when {
+        val token: Token = when {
             c.isDigit() || (c == '-' && nextChar(2).isDigit()) -> {
                 """-?\d+""".toRegex().find(sourceStr, i + 1)?.value?.run {
-                    this to Type.OPERAND
-                } as Pair<String, Type>
+                    Token(this, Type.OPERAND)
+                } as Token
             }
-            c in "+-*/" -> c.toString() to Type.OPERATOR
-            c == '(' -> c.toString() to Type.LEFT
-            c == ')' -> c.toString() to Type.RIGHT
+            c in "+-*/" -> Token(c.toString(), Type.OPERATOR)
+            c == '(' -> Token(c.toString(), Type.LEFT)
+            c == ')' -> Token(c.toString(), Type.RIGHT)
             else -> error("Unknown symbol in source string: ${sourceStr[i]}")
         }
-        move(token.first.length)
+        move(token.lexeme.length)
         return token
     }
 
@@ -84,26 +82,24 @@ class ParseTree(path: String) {
 
     private fun parseExpr(): Node {
         var curToken = tokenizer.next()
-        return when (curToken.second) {
+        return when (curToken.type) {
             Type.LEFT -> {
                 curToken = tokenizer.next()
-                if (curToken.second != Type.OPERATOR) {
-                    error("Expression parsing failed: expected operator after '(', but got ${curToken.first}")
+                if (curToken.type != Type.OPERATOR) {
+                    error("Expression parsing failed: expected operator after '(', but got ${curToken.lexeme}")
                 }
-                val x = Operator(curToken.first[0])
-                x.left = parseExpr()
-                x.right = parseExpr()
-                if (tokenizer.next().second != Type.RIGHT) {
-                    error("Expression parsing failed: expected ')' after two operands, but got ${curToken.first}")
+                val x = Operator(curToken.lexeme[0], parseExpr(), parseExpr())
+                if (tokenizer.next().type != Type.RIGHT) {
+                    error("Expression parsing failed: expected ')' after two operands, but got ${curToken.lexeme}")
                 }
                 x
             }
-            Type.OPERAND -> Operand(curToken.first.toInt())
-            else -> error("Parsing failed: expected an '(' or an operand, but got ${curToken.first}")
+            Type.OPERAND -> Operand(curToken.lexeme.toInt())
+            else -> error("Parsing failed: expected an '(' or an operand, but got ${curToken.lexeme}")
         }
     }
 
     fun compute(): Int = root.compute()
 
-    fun print() = root.print(0, ".")
+    override fun toString() = root.toString()
 }
--- src/test/kotlin/homework5/ParseTreeTest.kt
+++ src/test/kotlin/homework5/ParseTreeTest.kt
@@ -8,18 +8,56 @@ import org.junit.jupiter.params.provider.MethodSource
 class ParseTreeTest {
     companion object {
         @JvmStatic
-        fun inputData() = listOf<Arguments>(
+        fun inputDataCompute() = listOf<Arguments>(
             Arguments.of("test1", -11),
             Arguments.of("test2", 25),
             Arguments.of("test3", 40),
         )
+
+        @JvmStatic
+        fun inputDataPrint() = listOf<Arguments>(
+            Arguments.of("test1", """
+                *
+                ...+
+                ......-1
+                ......-10
+                .../
+                ......2
+                ......2
+            """.trimIndent()),
+            Arguments.of("test2", """
+                *
+                ...5
+                ...+
+                ......-
+                ........./
+                ............40
+                ............-8
+                .........10
+                ......20
+            """.trimIndent()),
+            Arguments.of("test3", """
+                +
+                ...20
+                ...20
+            """.trimIndent())
+        )
     }
 
-    @MethodSource("inputData")
+    @MethodSource("inputDataCompute")
     @ParameterizedTest
     fun testParse(testName: String, expected: Int) {
        ParseTree(this.javaClass.getResource("$testName.txt").path).apply {
+           println(this)
            assertEquals(expected, this.compute())
        }
+    }
+
+    @MethodSource("inputDataPrint")
+    @ParameterizedTest
+    fun testPrint(testName: String, expected: String) {
+        ParseTree(this.javaClass.getResource("$testName.txt").path).apply {
+            assertEquals(expected, this.toString())
+        }
     }
 }
