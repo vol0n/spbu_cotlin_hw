8249b799ba5257ebfa7bf8e2df9c4bf2b6d8c546
Volynets Arseniy <vol0ncar@yandex.ru>
2021-04-30 18:53:06 +0300

    Intial commit

--- src/main/kotlin/homework7/Matrix.kt
+++ src/main/kotlin/homework7/Matrix.kt
@@ -2,16 +2,14 @@ package homework7
 
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
-import kotlin.coroutines.coroutineContext
 
-class Matrix(val nrows: Int, val ncols: Int, init: (Int, Int) -> Int = {_, _ -> 0}) {
+class Matrix(val nrows: Int, val ncols: Int, init: (Int, Int) -> Int = { _, _ -> 0 }) {
     var data = Array(nrows) { i -> IntArray(ncols) { j -> init(i, j) } }
     fun dot(rowIndex: Int, colIndex: Int, other: Matrix) = other
         .data
-        .foldIndexed(0)
-        { sum, idx, row -> sum + row[colIndex] * data[rowIndex][idx] }
+        .foldIndexed(0) { sum, idx, row -> sum + row[colIndex] * data[rowIndex][idx] }
     suspend operator fun times(other: Matrix): Matrix {
+        require(ncols == other.nrows)
         val res = Matrix(nrows, other.ncols)
         coroutineScope {
             for (i in 0 until nrows) {
@@ -22,17 +20,8 @@ class Matrix(val nrows: Int, val ncols: Int, init: (Int, Int) -> Int = {_, _ -> 
                 }
             }
         }
-        return res;
+        return res
     }
 
     override fun toString() = data.joinToString("\n") { row -> row.joinToString(" ") }
-}
-
-fun main() = runBlocking {
-    val a = Matrix(2, 2) { i, j -> i+j }
-    val b = Matrix(2, 2) { i, j -> i+j }
-    println(a)
-    println(b)
-    val r = a*b
-    println(r)
 }
---
+++ src/test/kotlin/homework7/MatrixTest.kt
@@ -0,0 +1,5 @@
+package homework7
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class MatrixTest
