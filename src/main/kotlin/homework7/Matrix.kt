773b5d7561ef87e2c9c2f771319160aa8603661b
Volynets Arseniy <vol0ncar@yandex.ru>
2021-05-28 23:36:09 +0300

    Required changes: add bigger tests

--- src/main/kotlin/homework7/Matrix.kt
+++ src/main/kotlin/homework7/Matrix.kt
@@ -3,11 +3,13 @@ package homework7
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 
-class Matrix(val nrows: Int, val ncols: Int, init: (Int, Int) -> Int = { _, _ -> 0 }) {
-    var data = Array(nrows) { i -> IntArray(ncols) { j -> init(i, j) } }
-    fun dot(rowIndex: Int, colIndex: Int, other: Matrix) = other
+class Matrix(private val nrows: Int, private val ncols: Int, init: (Int, Int) -> Int = { _, _ -> 0 }) {
+    val data = Array(nrows) { i -> IntArray(ncols) { j -> init(i, j) } }
+
+    private fun dot(rowIndex: Int, colIndex: Int, other: Matrix) = other
         .data
-        .foldIndexed(0) { sum, idx, row -> sum + row[colIndex] * data[rowIndex][idx] }
+        .foldIndexed(0) { idx, sum, row -> sum + row[colIndex] * data[rowIndex][idx] }
+
     suspend operator fun times(other: Matrix): Matrix {
         require(ncols == other.nrows)
         val res = Matrix(nrows, other.ncols)
@@ -24,4 +26,13 @@ class Matrix(val nrows: Int, val ncols: Int, init: (Int, Int) -> Int = { _, _ ->
     }
 
     override fun toString() = data.joinToString("\n") { row -> row.joinToString(" ") }
+
+    constructor(nrows: Int, ncols: Int, vararg elems: Int) : this(nrows, ncols,
+        { i, j ->
+            require(elems.size == ncols * nrows) {
+                "Can't create matrix $nrows x $ncols with ${elems.size} elements"
+            }
+            elems[i * nrows + j]
+        }
+    )
 }
--- src/test/kotlin/homework7/MatrixTest.kt
+++ src/test/kotlin/homework7/MatrixTest.kt
@@ -12,16 +12,53 @@ internal class MatrixTest {
         fun testDataMul() = listOf<Arguments> (
             Arguments.of(Matrix(0, 0), Matrix(0, 0), Matrix(0, 0)),
             Arguments.of(Matrix(1, 1), Matrix(1, 1), Matrix(1, 1)),
-            Arguments.of(Matrix(1, 1) { _, _ -> 3}, Matrix(1, 1) { _, _ -> 2},
-                Matrix(1, 1) { _, _ -> 6}),
-            Arguments.of(Matrix(1, 2) { _, j -> j + 1}, Matrix(2, 2) { i, j -> i + j},
-                Matrix(1, 2) { _, j -> 3*j + 2})
+            Arguments.of(Matrix(1, 1) { _, _ -> 6},
+                Matrix(1, 1) { _, _ -> 3},
+                Matrix(1, 1) { _, _ -> 2},
+                ),
+            Arguments.of(Matrix(1, 2) { _, j -> 3*j + 2},
+                Matrix(1, 2) { _, j -> j + 1},
+                Matrix(2, 2) { i, j -> i + j},
+            ),
+            Arguments.of(
+                Matrix(3, 3,
+                    17, -155, -260,
+                    -47, -82, 364,
+                    6, 30, 130
+                ),
+                Matrix(3, 3,
+                    1, -10, -13,
+                    -20, -1, 13,
+                    -2,  8, 0),
+                Matrix(3, 3,
+                    1, 9, -13,
+                    1, 6, 13,
+                    -2,  8, 9),
+            ),
+            Arguments.of(
+                Matrix(5, 5,
+                    -13, -21, 4, -13, 24,
+                    13, 21, -4, 13, -24,
+                    31, 3, 40, 27, 36,
+                    10, 5, -7, 12, -31,
+                    -9, -8, 7, -3, 27
+                ),
+                Matrix(5, 5,
+                    1, 2, 3, 4, 5,
+                    -1, -2, -3, -4, -5,
+                    1, -2, 3, -4, 5,
+                    -5, -2, -3, -3, 1,
+                    1, -1, 2, 3, 2
+                ), null
+            )
+
         )
     }
 
     @MethodSource("testDataMul")
     @ParameterizedTest
-    fun testMul(op1: Matrix, op2: Matrix, expected: Matrix) = runBlocking {
-        assertArrayEquals(expected.data, (op1 * op2).data)
+    fun testMul(expected: Matrix, op1: Matrix, op2: Matrix?) = runBlocking {
+        val operand2 = op2 ?: op1
+        assertArrayEquals(expected.data, (op1 * operand2).data)
     }
 }
