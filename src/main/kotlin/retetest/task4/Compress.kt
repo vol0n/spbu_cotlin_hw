880475e7ba6d38d682e8a1b2ac97812c278571e8
Volynets Arseniy <vol0ncar@yandex.ru>
2021-06-04 14:23:39 +0300

    Fix one error

--- src/main/kotlin/retetest/task4/Compress.kt
+++ src/main/kotlin/retetest/task4/Compress.kt
@@ -2,7 +2,6 @@ package retetest.task4
 
 fun ByteArray.compress(): ByteArray {
     if (this.isEmpty()) return ByteArray(0)
-    if (this.size == 1) return byteArrayOf(1, this[0])
     var res = ByteArray(0)
     this.foldIndexed(1) { index, count, currentByte ->
         val isCurrentEqualToNext = if (index < this.size - 1) currentByte == this[index + 1] else false
